47b8b07ad91a01fc4a28e8af982490820785a28f
shihabsaleem <shihabchikkushihab@gmail.com>
2023-03-01 18:34:43 +0530

    navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Search } from "@mui/icons-material";
+import React from "react";
+import "./Style/navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-Container">
+      <div className="navbar-Wrapper">
+        <div className="navbar-Left">
+          <div className="lang">EN</div>
+          <div className="searchNav">
+            Input
+            <Search />
+          </div>
+        </div>
+        <div className="navbar-Center">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem
+          nostrum reprehenderit saepe ipsam cupiditate earum iure cumque, sequi
+          quibusdam optio rem quo placeat mollitia ipsa necessitatibus
+          temporibus repellat, sunt vitae.
+        </div>
+        <div className="navbar-Right">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+          molestias nesciunt dolorum, in deserunt eum accusamus consectetur
+          corporis dolorem veniam possimus sapiente dignissimos commodi,
+          distinctio officia, et magni rerum minus?
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
