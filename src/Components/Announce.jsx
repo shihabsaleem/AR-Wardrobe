e40ab3e031a646b58759fe28d325313dd3ea832d
shihabsaleem <shihabchikkushihab@gmail.com>
2023-03-04 08:08:35 +0530

    ticker

---
+++ src/Components/Announce.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Style/announce.scss";
+
+const Announce = () => {
+  return (
+    <div className="container">Let's Have Hybrid Shopping Experience....</div>
+  );
+};
+
+export default Announce;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./../Components/Style/home.scss"
 import Navbar from '../Components/Navbar'
+import Announce from '../Components/Announce'
 
 const Home = () => {
   return (
     <div>
+      <Announce/>
       <Navbar/>
     </div>
   )
