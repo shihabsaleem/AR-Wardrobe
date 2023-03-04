7d1e75d260aaf049142c16d034c4b5b58cfd75f3
shihabsaleem <shihabchikkushihab@gmail.com>
2023-03-04 16:53:12 +0530

    commit

--- src/data.js
+++ src/data.js
@@ -5,21 +5,21 @@ import slider2 from "./Assets/img2.png";
 export const sliderItems = [
   {
     id: 1,
-    img: { slider0 },
+    img: "slider0",
     title: "Autumn Sale",
     desc: "lorem ipsum dolor sit amet",
     bg: "#e7ffff",
   },
   {
     id: 2,
-    img: { slider1 },
+    img: "slider1",
     title: "Summer Sale",
     desc: "lorem ipsum dolor sit amet",
     bg: "#e7ffff",
   },
   {
     id: 3,
-    img: { slider2 },
+    img: "slider2",
     title: "Winter Sale",
     desc: "lorem ipsum dolor sit amet",
     bg: "#e7ffff",
