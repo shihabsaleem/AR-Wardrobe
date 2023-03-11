c73527c81c9b5d1b357cd6d92ef8f7da031cd230
shihabsaleem <shihabchikkushihab@gmail.com>
2023-03-11 13:22:17 +0530

    footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import "./../Components/Style/home.scss"
-import Navbar from '../Components/Navbar'
-import Announce from '../Components/Announce'
-import Slider from '../Components/Slider'
-import Categories from '../Components/Categories'
+import React from "react";
+import "./../Components/Style/home.scss";
+import Navbar from "../Components/Navbar";
+import Announce from "../Components/Announce";
+import Slider from "../Components/Slider";
+import Categories from "../Components/Categories";
+import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Announce/>
-      <Navbar/>
-      <Slider/>
-      <Categories/>
+    <div className="home">
+      <Announce />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
