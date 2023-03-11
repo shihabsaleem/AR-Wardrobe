2a344a3a685ab1d9fe6f25cf9fe04b151a13f7a0
shihabsaleem <shihabchikkushihab@gmail.com>
2023-03-11 13:58:06 +0530

    error page

--- src/Pages/NoPage.jsx
+++ src/Pages/NoPage.jsx
@@ -1,17 +1,14 @@
-import { height } from "@mui/system";
 import React from "react";
 import error404 from "./../Assets/error404.svg";
-<style>
-
-.notfound{
-    height
-}
-    </style>
+import "./../Components/Style/error.scss";
 
 const NoPage = () => {
   return (
-    <div className="notfound">
-      <img src={error404} alt="crdit rafiki" />
+    <div className="center">
+      <div className="notfound">
+        <img src={error404} height="500px" alt="crdit rafiki" />
+        <p>The Page Not Found</p>
+      </div>
     </div>
   );
 };
