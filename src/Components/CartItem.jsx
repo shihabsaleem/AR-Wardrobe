f2bda205ce2fc1d8265e001e95064485305bc1ea
shihabsaleem <shihabchikkushihab@gmail.com>
2023-03-18 12:48:51 +0530

    cart

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Style/cart.scss";
+import { item } from "../data";
 
 const CartItem = () => {
   const [items, setItems] = useState([]);
@@ -31,18 +32,32 @@ const CartItem = () => {
         <ul>
           {items.map((item, index) => (
             <li key={index}>
-              <img src={item.image} alt={item.name} />
+              <img src={item.image} alt={item.title} />
               <div>
-                <h3>{item.name}</h3>
+                <h3>{item.title}</h3>
                 <p>{item.description}</p>
-                <p>${item.price.toFixed(2)}</p>
+                <p>{item.price}</p>
                 <button onClick={() => removeItem(index)}>Remove</button>
               </div>
             </li>
           ))}
         </ul>
       )}
-      <h3>Total: ${calculateTotal().toFixed(2)}</h3>
+      <h3>Total: {calculateTotal()}</h3>
+      <h3>Available items:</h3>
+      <ul>
+        {item.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <div>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>{product.price}</p>
+              <button onClick={() => addItem(product)}>Add to cart</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
