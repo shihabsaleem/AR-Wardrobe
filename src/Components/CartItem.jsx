4e94b85ac12022350a4afcc8a7bd23d65d0ee125
shihabsaleem <shihabchikkushihab@gmail.com>
2023-03-18 13:33:33 +0530

    cart

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -26,58 +26,54 @@ const CartItem = () => {
   return (
     <div className="cart">
       <h2>Shopping Cart</h2>
-      {items.length === 0 ? (
-        <p>No items in cart.</p>
-      ) : (
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>
-              <img src={item.image} alt={item.title} />
-              <div>
-                <h3>{item.title}</h3>
-                <p>{item.description}</p>
-                <p>
-                  {item.price} x {item.quantity}
-                </p>
-                <button onClick={() => removeItem(index)}>Remove</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-      <h3>Total: {calculateTotal()}</h3>
-      <h3>Available items:</h3>
-      <ul>
-        {item.map((product) => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <div>
-              <h3>{product.title}</h3>
-              <p>{product.description}</p>
-              <p>{product.price}</p>
-              <div>
-                <label htmlFor={`quantity_${product.id}`}>Quantity:</label>
-                <select
-                  id={`quantity_${product.id}`}
-                  defaultValue="1"
-                  onChange={(event) =>
-                    addItem(product, parseInt(event.target.value))
-                  }
-                >
-                  {[1, 2, 3, 4, 5].map((quantity) => (
-                    <option key={quantity} value={quantity}>
-                      {quantity}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <button onClick={() => addItem(product, 1)}>Add to cart</button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="cart-container">
+        <div className="cart-items">
+          {items.length === 0 ? (
+            <p>No items in cart.</p>
+          ) : (
+            <ul>
+              {items.map((item, index) => (
+                <li key={index}>
+                  <img src={item.image} alt={item.title} />
+                  <div>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                    <button onClick={() => removeItem(index)}>Remove</button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+          <h3>Total: {calculateTotal()}</h3>
+        </div>
+        <div className="cart-controls">
+          <h3>Available items:</h3>
+          <ul>
+            {item.map((product) => (
+              <li key={product.id}>
+                <img src={product.image} alt={product.title} />
+                <div>
+                  <h3>{product.title}</h3>
+                  <p>{product.description}</p>
+                  <p>{product.price}</p>
+                </div>
+                <div className="controls">
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </select>
+                  <button onClick={() => addItem(product)}>Add to cart</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
+  
 };
 
 export default CartItem;
